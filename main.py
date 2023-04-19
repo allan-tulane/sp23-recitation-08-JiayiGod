8576ca6983ec7719e2182088f52341f7db0fc8ae
jiaaaai <123215655+JiayiGod@users.noreply.github.com>
2023-04-18 21:29:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 
 ####### Problem 3 #######
 
-test_cases = [('book', 'back'), ('kookaburra', 'kookybird'), ('elephant', 'relevant'), ('AAAGAATTCA', 'AAATCA')]
-alignments = [('book', 'back'), ('kookaburra', 'kookybird-'), ('relev-ant','-elephant'), ('AAAGAATTCA', 'AAA---T-CA')]
+test_cases = [('book', 'back'), ('kookaburra', 'kookybird'), ('relevant','elephant'), ('AAAGAATTCA', 'AAATCA')]
+alignments = [('boo--k', 'b--ack'), ('kooka-bu-rra-', 'kook-yb-ir--d'), ('relev--ant','-ele-phant'), ('AAAGAATTCA', 'AAA---T-CA')]
 
 def MED(S, T):
-    # TO DO - modify to account for insertions, deletions and substitutions
     if (S == ""):
         return(len(T))
     elif (T == ""):
@@ -19,10 +18,55 @@ def MED(S, T):
 
 def fast_MED(S, T, MED={}):
     # TODO -  implement memoization
+    if (S, T) in MED:
+        return MED[(S, T)]
+    elif len(S)==0:
+        MED[(S, T)] = len(T)
+    elif len(T)==0:
+        MED[(S, T)] = len(S)
+    else:
+        if (S[0] == T[0]):
+            MED[(S, T)] = fast_MED(S[1:], T[1:])
+        else:
+            MED[(S, T)] = 1 + min(fast_MED(S, T[1:]), fast_MED(S[1:], T))
+    return MED[(S, T)]
     pass
 
 def fast_align_MED(S, T, MED={}):
     # TODO - keep track of alignment
+    align_S=""
+    align_T=""
+    while True:
+        # three cases when editing
+        same=fast_MED(S,T)
+        insert=fast_MED(S, T[:-1])
+        delete=fast_MED(S[:-1], T)
+
+        # do the diogonal backtrace
+        min_=min(same,insert,delete)
+        
+        if min_==same:
+            # backtrace with no need to change
+            align_S=S[-1]+align_S
+            align_T=T[-1]+align_T       
+            S=S[:-1]
+            T=T[:-1]
+        else:
+            # backtrace is either insertion or deletion based on minimum
+            if min_==insert:
+                align_S="-"+align_S
+                align_T=T[-1]+align_T
+                T=T[:-1]  
+            elif min_==delete:
+                align_T="-"+align_T
+                align_S=S[-1]+align_S           
+                S=S[:-1]
+
+        if len(S)==0 or len(T)==0:
+            # when one of the letter is empty
+            align_S="-"*len(T)+S+align_S
+            align_T="-"*len(S)+T+align_T
+            return align_S,align_T
     pass
 
 def test_MED():
